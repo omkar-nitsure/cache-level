a4baaea810f0b58f32e37fa5eb1a9c91fd524712
omkar <omkarnitsure2003@gmail.com>
2024-11-12 16:30:25 +0530

    predictor structure

--- ChampSim/scripts/download_dpc3_traces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PWD/../dpc3_traces
-while read LINE
-do
-    wget -P $PWD/../dpc3_traces -c https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/$LINE
-done < dpc3_max_simpoint.txt
--- Hermes/scripts/create_jobfile.pl
+++ Hermes/scripts/create_jobfile.pl
@@ -2,137 +2,89 @@
 
 use warnings;
 use Getopt::Long;
-use Trace;
-use Exp;
 
-# defaults
-my $tlist_file;
-my $exp_file;
-my $exe;
-my $local = "0";
+# Default parameters
+my $exe = "../../bin/glc-perceptron-no-no-no-no-multi-1core-1ch";
+my $local = "1";
 my $num_parallel = 8;
 my $ncores = 1;
 my $slurm_partition = "slurm_part";
 my $hostname = "kratos";
-my $exclude_list;
-my $include_list;
 my $extra;
 
-GetOptions('tlist=s' => \$tlist_file,
-	   'exp=s' => \$exp_file,
-	   'exe=s' => \$exe,
-	   'ncores=s' => \$ncores,
-	   'local=s' => \$local,
-	   'num_parallel=s' => \$num_parallel,
-	   'partition=s' => \$slurm_partition,
-	   'hostname=s' => \$hostname,
-	   'exclude=s' => \$exclude_list,
-	   'include=s' => \$include_list,
-	   'extra=s' => \$extra,
-) or die "Usage: $0 --exe <executable> --exp <exp file> --tlist <trace list>\n";
+# Get any command-line options
+GetOptions(
+    'exe=s' => \$exe,
+    'ncores=s' => \$ncores,
+    'local=s' => \$local,
+    'num_parallel=s' => \$num_parallel,
+    'partition=s' => \$slurm_partition,
+    'hostname=s' => \$hostname,
+    'extra=s' => \$extra,
+) or die "Usage: $0 --exe <executable>\n";
 
+# Check if HERMES_HOME is set
 die "\$HERMES_HOME env variable is not defined.\nHave you sourced setvars.sh?\n" unless defined $ENV{'HERMES_HOME'};
 
-die "Supply exe\n" unless defined $exe;
-die "Supply tlist\n" unless defined $tlist_file;
-die "Supply exp\n" unless defined $exp_file;
+# Hardcoded trace configurations
+my @trace_info = (
+    { NAME => "bwaves_98B.trace", TRACE => "$ENV{'HERMES_HOME'}/traces/bwaves_98B.trace.xz", KNOBS => "--warmup_instructions=1000000 --simulation_instructions=1000000" },
+);
 
-my $exclude_nodes_list = "";
-$exclude_nodes_list = "$hostname\[$exclude_list\]" if defined $exclude_list;
-my $include_nodes_list = "";
-$include_nodes_list = "$hostname\[$include_list\]" if defined $include_list;
+# Hardcoded experiment configurations
+my @exp_info = (
+    { NAME => "nopref", KNOBS => "--warmup_instructions=100000000 --simulation_instructions=500000000 --llc_replacement_type=ship --config=$ENV{'HERMES_HOME'}/config/nopref.ini --num_rob_partitions=3 --rob_partition_size=64,128,320 --rob_frontal_partition_ids=0 --rob_dorsal_partition_ids=2" },
+    { NAME => "pythia", KNOBS => "--warmup_instructions=100000000 --simulation_instructions=500000000 --llc_replacement_type=ship --config=$ENV{'HERMES_HOME'}/config/pythia.ini --scooby_enable_direct_pref_issue=true --scooby_pref_at_lower_level=true --scooby_dyn_degrees_type2=1,1,2,4" },
+    # Add additional experiment configurations here if needed
+);
 
-my @trace_info = Trace::parse($tlist_file);
-my @exp_info = Exp::parse($exp_file);
-if($ncores == 0)
-{
-	print "have to supply -ncores\n";
-	exit 1;
+# Output SBATCH script preamble
+if ($local eq "0") {
+    print "#!/bin/bash -l\n#\n# SLURM job script\n";
+} else {
+    print "#!/bin/bash\n#\n# Local job script\n";
 }
-
-# preamble for sbatch script
-if($local eq "0")
-{
-	print "#!/bin/bash -l\n";
-	print "#\n";
-	print "#\n";
+print "#\n# Traces:\n";
+foreach my $trace (@trace_info) {
+    my $trace_name = $trace->{"NAME"};
+    print "#    $trace_name\n";
 }
-else
-{
-	print "#!/bin/bash\n";
-	print "#\n";
-	print "#\n";
-}
-
-print "#\n";
-print "# Traces:\n";
-foreach $trace (@trace_info)
-{
-	my $trace_name = $trace->{"NAME"};
-	print "#    $trace_name\n";
+print "#\n# Experiments:\n";
+foreach my $exp (@exp_info) {
+    my $exp_name = $exp->{"NAME"};
+    my $exp_knobs = $exp->{"KNOBS"};
+    print "#    $exp_name: $exp_knobs\n";
 }
 print "#\n";
 
-print "#\n";
-print "# Experiments:\n";
-foreach $exp (@exp_info)
-{
-	my $exp_name = $exp->{"NAME"};
-	my $exp_knobs = $exp->{"KNOBS"};
-	print "#    $exp_name: $exp_knobs\n";
-}
-print "#\n";
-print "#\n";
-print "#\n";
-print "#\n";
-
+# Run each trace and experiment configuration
 my $count = 0;
+foreach my $trace (@trace_info) {
+    foreach my $exp (@exp_info) {
+        my $exp_name = $exp->{"NAME"};
+        my $exp_knobs = $exp->{"KNOBS"};
+        my $trace_name = $trace->{"NAME"};
+        my $trace_input = $trace->{"TRACE"};
+        my $trace_knobs = $trace->{"KNOBS"};
 
-foreach $trace (@trace_info)
-{
-	foreach $exp (@exp_info)
-	{
-		my $exp_name = $exp->{"NAME"};
-		my $exp_knobs = $exp->{"KNOBS"};
-		my $trace_name = $trace->{"NAME"};
-		my $trace_input = $trace->{"TRACE"};
-		my $trace_knobs = $trace->{"KNOBS"};
+        # Define command based on local or SLURM execution
+        my $cmdline;
+        if ($local) {
+            $cmdline = "$exe $exp_knobs $trace_knobs -traces $trace_input > ${trace_name}_${exp_name}.out 2>&1";
+            $count++;
+            if ($count % $num_parallel) {
+                $cmdline = $cmdline . ' &';
+            }
+        } else {
+            my $slurm_cmd = "sbatch -p $slurm_partition --mincpus=1";
+            $slurm_cmd .= " --nodelist=${hostname}" if defined $hostname;
+            $slurm_cmd .= " $extra" if defined $extra;
+            $slurm_cmd .= " -c $ncores -J ${trace_name}_${exp_name} -o ${trace_name}_${exp_name}.out -e ${trace_name}_${exp_name}.err";
+            $cmdline = "$slurm_cmd $ENV{'HERMES_HOME'}/wrapper.sh $exe \"$exp_knobs $trace_knobs -traces $trace_input\"";
+        }
 
-		my $cmdline;
-		if($local)
-		{
-			$cmdline = "$exe $exp_knobs $trace_knobs -traces $trace_input > ${trace_name}_${exp_name}.out 2>&1";
-			$count++;
-			if($count % $num_parallel)
-			{
-				$cmdline = $cmdline . ' &';
-			}
-		}
-		else
-		{
-			$slurm_cmd = "sbatch -p $slurm_partition --mincpus=1";
-			if (defined $include_list)
-			{
-				$slurm_cmd = $slurm_cmd." --nodelist=${include_nodes_list}";
-			}
-			if (defined $exclude_list)
-			{
-				$slurm_cmd = $slurm_cmd." --exclude=${exclude_nodes_list}";
-			}
-			if (defined $extra)
-			{
-				$slurm_cmd = $slurm_cmd." $extra";
-			}
-			$slurm_cmd = $slurm_cmd." -c $ncores -J ${trace_name}_${exp_name} -o ${trace_name}_${exp_name}.out -e ${trace_name}_${exp_name}.err";
-			$cmdline = "$slurm_cmd $ENV{'HERMES_HOME'}/wrapper.sh $exe \"$exp_knobs $trace_knobs -traces $trace_input\"";
-		}
-		
-		# Additional hook replace
-		$cmdline =~ s/\$\(HERMES_HOME\)/$ENV{'HERMES_HOME'}/g;
-		$cmdline =~ s/\$\(EXP\)/$exp_name/g;
-		$cmdline =~ s/\$\(TRACE\)/$trace_name/g;
-		$cmdline =~ s/\$\(NCORES\)/$ncores/g;
-		
-		print "$cmdline\n";
-	}
+        # Print and execute command
+        print "$cmdline\n";
+    }
 }
+
